f9a1a97a3226247583b593e44e4b16fdfdabf961
Sahil Ladola <110282930+sahil-ladola@users.noreply.github.com>
2024-01-31 12:53:23 +0530

    Add files via upload
    pixela API to track habit

---
+++ Habit Tracking/main.py
@@ -0,0 +1,48 @@
+# https://pixe.la/
+import requests
+from datetime import datetime
+USERNAME = "sahilladola"
+TOKEN = "wedndf4bdgjtdsd2sf3vsd"
+GRAPH_ID = "graph1"
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+graph_params = {
+    "id": GRAPH_ID,
+    "name": "Study Graph",
+    "unit": "min",
+    "type": "int",
+    "color": "sora",
+}
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+# response = requests.post(url=graph_endpoint, json=graph_params, headers=headers)
+# print(response.text)
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime.now()
+
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": input("How many minutes did you study today? "),
+}
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+print(response.text)
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+update_pixel_data = {
+    "quantity": "300"
+}
+# response = requests.put(url=update_endpoint, json=update_pixel_data, headers=headers)
+# print(response.text)
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+response = requests.delete(url=delete_endpoint, headers=headers)
