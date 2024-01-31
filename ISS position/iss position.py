4d41a180720f86799eabad76537fe18b7d3d3639
Sahil Ladola <110282930+sahil-ladola@users.noreply.github.com>
2024-01-31 12:33:09 +0530

    Add files via upload
    learned how to use API, API ENDPOINTS, whenever ISS overhead send email

---
+++ ISS position/iss position.py
@@ -0,0 +1,50 @@
+import time
+import requests
+from datetime import datetime
+import smtplib
+
+LAT = 21.170240
+LNG = 72.831062
+
+
+def iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+
+    data = response.json()
+
+    longitude = float(data["iss_position"]["longitude"])
+    latitude = float(data["iss_position"]["latitude"])
+    iss_position = (longitude, latitude)
+
+    if LAT-5 <= latitude <= LAT+5 and LNG-5 <= longitude <= LNG+5:
+        return True
+
+
+def is_night():
+    parameters = {
+        "lat": LAT,
+        "lng": LNG,
+        "formatted": 0,
+    }
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    current_hour = datetime.now().hour
+
+    if current_hour >= sunset or current_hour <= sunrise:
+        return True
+
+
+while True:
+    time.sleep(60)
+    if iss_overhead() and is_night():
+        with smtplib.SMTP("smtp.gmail.com",587) as connection:
+            connection.starttls()
+            connection.login("20bmiit019@gmail.com", "lfvhtfputojkurlx")
+            connection.sendmail(from_addr="20bmiit019@gmail.com",
+                                to_addrs="sahilladola18@gmail.com",
+                                msg="Subject:ISS IS OVERHEAD\n\nLOOK UP 👆🏻")
